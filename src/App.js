185583ebfef8f0c540f5afa613b36fe369e68696
Andras Olah <andras.antal.olah@hu.ibm.com>
2021-02-03 00:00:47 +0100

    Added state (Section 3, lecture 38-39)

--- src/App.js
+++ src/App.js
@@ -7,17 +7,27 @@ import { PersonAsClassBasedComponent as Pacbc } from './Person/Person.js';
 
 // "App" is the root component, that can have infinite nested child components
 class App extends Component {
+  //state is a reserved word in class that extends Component
+  state = {
+    persons: [
+      {name:'Andras', age: 11},
+      {name:'Tibor', age: 22},
+      {name:'Klara', age: 33},
+    ],
+    otherKey: 'otherValue'
+  }
   render() {
     return (
       // this is JSX code that will be compiled automatically when added to the DOM
       <div className="App">
         <h1>This is React inside App div</h1>
         <p>This is a paragraph inside of main div</p>
-        <PersonButCanBeAnything name="Andras" age="11" />
-        <PersonButCanBeAnything name="Tibor" age="22">
-          BlöBlö
+        <PersonButCanBeAnything name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <PersonButCanBeAnything name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <PersonButCanBeAnything name={this.state.persons[2].name} age={this.state.persons[2].age} >
+          This is inside third person.
         </PersonButCanBeAnything>
-        <PersonButCanBeAnything name="Klara" age="33" />
+
         <Pacbc name="Pacb" age="00">
           This is inside Pacbc element
         </Pacbc>
