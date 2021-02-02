377d8f38105014cd46a0faaa3348f0c3ea554312
Andras Olah <andras.antal.olah@hu.ibm.com>
2021-02-02 23:44:56 +0100

    Add props and props.children - Section 3, lecture 37

--- src/App.js
+++ src/App.js
@@ -3,27 +3,32 @@ import './App.css';
 // this is using the default export from Person.js
 import PersonButCanBeAnything from './Person/Person'; //Person.js extension can be ommitted
 // importing a specific class needs to be inside {} brackets
-import {PersonAsClassBasedComponent as Pacbc} from './Person/Person.js';
+import { PersonAsClassBasedComponent as Pacbc } from './Person/Person.js';
 
 // "App" is the root component, that can have infinite nested child components
 class App extends Component {
   render() {
     return (
-    // this is JSX code that will be compiled automatically when added to the DOM 
-    <div className="App">
-    <h1>This is React inside App div</h1>
-    <p>This is a paragraph inside of main div</p>
-    <PersonButCanBeAnything/>
-    <Pacbc/>
-    </div>
-    // this is not allowed, the jsx should return only one root element
-    // <p> paragraph outside of main div</p>
+      // this is JSX code that will be compiled automatically when added to the DOM
+      <div className="App">
+        <h1>This is React inside App div</h1>
+        <p>This is a paragraph inside of main div</p>
+        <PersonButCanBeAnything name="Andras" age="11" />
+        <PersonButCanBeAnything name="Tibor" age="22">
+          BlöBlö
+        </PersonButCanBeAnything>
+        <PersonButCanBeAnything name="Klara" age="33" />
+        <Pacbc name="Pacb" age="00">
+          This is inside Pacbc element
+        </Pacbc>
+      </div>
+      // this is not allowed, the jsx should return only one root element
+      // <p> paragraph outside of main div</p>
     );
     // the above gets compiled (in the background) to this code:
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello again!'));
   }
 }
-
 
 // default export is if the file is imported, this class will be exported
 // index.js does import this file, class:  import App from './App';
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 
 //Functional components (also referred to as "presentational", "dumb" or "stateless" components
 
-//convention - function name is in lowercase
+//convention - function name is in lowercase, properties variable name 'props'
 // using ES6 arrow function syntax arguments => function body
-const person = () => {
-    // this is a JSX syntax, we need to import React
-    return <p>This is rendered from a functional component</p>
-}
+const person = (props) => {
+  // this is a JSX syntax, we need to import React
+  return (
+    <div className="FunctionalComponent">
+      <p>
+        Name={props.name}, age={props.age}, random number=
+        {Math.floor(Math.random() * 30)}, something undefined={props.blabla}
+      </p>
+       <p>{props.children}</p> 
+    </div>
+  );
+};
 
 //Each component needs to return/ render some JSX code - it defines which HTML code React should render to the real DOM in the end.
 export class PersonAsClassBasedComponent extends Component {
-    render(){
-        return <p>This is renderd from a class based component</p>
-    }
+  render() {
+    return (
+      <div className="ClassBasedComponent">
+          {/* This is a comment inside a JSX */}
+          {/* props must be referenced as this.props*/}
+        <p>Name={this.props.name}, age={this.props.age}</p>
+        <p>Props.children content = {this.props.children}</p>
+      </div>
+    );
+  }
 }
 
 export default person;
