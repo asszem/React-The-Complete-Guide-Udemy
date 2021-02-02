db79970314942c14c334f481ba6b21f4eafa4612
Andras Olah <andras.antal.olah@hu.ibm.com>
2021-02-02 10:35:30 +0100

    Update App.js to match course example used in Section 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
+    // this is JSX code that will be compiled automatically when added to the DOM 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>This is React inside App div</h1>
     </div>
-  );
+    );
+    // the above gets compiled (in the background) to this code:
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello again!'));
+  }
 }
 
+
+// default export is if the file is imported, this class will be exported
+// index.js does import this file, class:  import App from './App';
 export default App;
