f8fde8ce56b668103e4b4f4d4cab87e616fe223c
András <asszem78@gmail.com>
2021-05-07 21:12:50 +0200

    WIP on Section 8 own solution - adding a Modal

--- src/assignments/NewContent/Section8/Own/components/Modal.js
+++ src/assignments/NewContent/Section8/Own/components/Modal.js
@@ -1,9 +1,19 @@
+import classes from './Modal.module.css';
+
 const Modal = (props) => {
-    return (
-        <div>
-            I'm Modal!
-        </div>
-    );
-}
+    console.log('visible: ', props.visible)
+  return (
+    <div
+      className={classes.Modal}
+      style={{
+        transform: props.visible ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.visible ? '1' : '0',
+      }}
+    >
+      {props.message}
+      <button onClick={props.closeModalHandler}>Close</button>
+    </div>
+  );
+};
 
 export default Modal;
--- src/assignments/NewContent/Section8/Own/containers/Section8OwnApp.js
+++ src/assignments/NewContent/Section8/Own/containers/Section8OwnApp.js
@@ -1,59 +1,75 @@
 import React, { useState } from 'react';
 import AddUser from '../components/AddUser';
 import ListUsers from '../components/ListUsers';
+import Modal from '../components/Modal';
 
 const Section8OwnApp = () => {
-    const [users, setUsers] = useState(
-        [
-            {
-                name: 'Andras',
-                age: 42,
-                id: 1,
-            }
-        ]);
+  const [users, setUsers] = useState([
+    {
+      name: 'Andras',
+      age: 42,
+      id: 1,
+    },
+  ]);
 
-    const onNewUserSubmitted = (newUserData) => {
-        let errorMsg = '';
-        if (newUserData.name === '') {
-            errorMsg = 'Empty name';
-        }
-        if (newUserData.age <= 0) {
-            errorMsg = 'Invalid age';
-        }
-        if (errorMsg !== '') {
-            alert(errorMsg);
-        } else {
-            const newId = getMaxId(users) + 1;
-            setUsers(
-                (prevUsers) => (
-                    [
-                        ...prevUsers,
-                        {
-                            name: newUserData.name,
-                            age: newUserData.age,
-                            id: newId,
-                        }
-                    ]
-                )
-            )
+  const [modal, setModal] = useState({
+    visible: false,
+    message: '',
+  });
 
-        }
-
+  const onNewUserSubmitted = (newUserData) => {
+    let errorMsg = '';
+    if (newUserData.name === '') {
+      setModal({
+        visible: true,
+        message: 'Empty name',
+      });
+    } else if (newUserData.age <= 0) {
+      setModal({
+        visible: true,
+        message: 'Invalid age',
+      });
+    } else {
+      const newId = getMaxId(users) + 1;
+      setUsers((prevUsers) => [
+        ...prevUsers,
+        {
+          name: newUserData.name,
+          age: newUserData.age,
+          id: newId,
+        },
+      ]);
     }
+  };
 
-    function getMaxId(users) {
-        return Math.max.apply(Math, users.map(function (o) { return o.id; }))
-    }
+  const closeModalHandler = () => {
+    setModal({ visible: false, message: 'Modal is closed' });
+  };
 
-    return (
-        <div>
-            <div>
-                <AddUser onNewUserSubmitted={onNewUserSubmitted}></AddUser>
-            </div>
-            <div>
-                <ListUsers users={users}></ListUsers></div>
-        </div>
+  function getMaxId(users) {
+    return Math.max.apply(
+      Math,
+      users.map(function (o) {
+        return o.id;
+      })
     );
-}
+  }
+
+  return (
+    <div>
+      <Modal
+        closeModalHandler={closeModalHandler}
+        message={modal.message}
+        visible={modal.visible}
+      ></Modal>
+      <div>
+        <AddUser onNewUserSubmitted={onNewUserSubmitted}></AddUser>
+      </div>
+      <div>
+        <ListUsers users={users}></ListUsers>
+      </div>
+    </div>
+  );
+};
 
 export default Section8OwnApp;
